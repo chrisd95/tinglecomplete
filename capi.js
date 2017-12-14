edf8792fc158be7b7dab5ddf354fab78b59704c0
chrisd95 <34041303+chrisd95@users.noreply.github.com>
2017-12-14 18:27:44 -0500

    Create capi.js

---
+++ capi.js
@@ -0,0 +1,19 @@
+var fs = require('fs');
+var jsonQuery = require('json-query')
+
+const request = require('request')
+request.get('https://api.bitfinex.com/v1/pubticker/BTCUSD',
+  function(error, response, body) {
+    var data = JSON.stringify(body);
+    fs.writeFileSync('cointicker.json', body, finished);
+      function finished(err){
+      }
+})
+
+
+var data = fs.readFileSync('cointicker.json');
+var cointicker = JSON.parse(data)
+
+console.log(cointicker);
+
+console.log('BTCUSD price was updated.')
